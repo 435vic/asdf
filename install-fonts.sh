01bd391876166b9883fa16261f21c4045d6382d3
Victor Javier <victor_javier2004@hotmail.com>
2018-10-19 10:59:00 -0500

    fix permission issue

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,6 +1,6 @@
 echo "getting Fira Mono for Powerline..."
 sudo mkdir -p /usr/share/fonts/opentype
-curl -L https://github.com/powerline/fonts/raw/master/FiraMono/FuraMono-Regular%20Powerline.otf > /usr/share/fonts/opentype/FuraMono-Powerline.otf
+curl -L https://github.com/powerline/fonts/raw/master/FiraMono/FuraMono-Regular%20Powerline.otf | sudo tee /usr/share/fonts/opentype/FuraMono-Powerline.otf
 echo "getting Fira Code..."
 mkdir -p ~/tmp/firacode
 curl -L https://github.com/tonsky/FiraCode/releases/download/1.206/FiraCode_1.206.zip > ~/tmp/FiraCode_1.206.zip
