78b7eeec27a651ec48adeab1616842cce208ee0a
Victor Javier <victor_javier2004@hotmail.com>
2018-10-20 15:53:51 -0500

    install jq

--- vscode-build/get-deps.sh
+++ vscode-build/get-deps.sh
@@ -2,7 +2,7 @@ echo "updating and upgrading..."
 sudo apt update
 sudo apt upgrade
 echo "Installing apt packages..."
-sudo apt install -y libsecret-1-dev git python2.7 clang make libx11-dev libxkbfile-dev fakeroot rpm
+sudo apt install -y jq libsecret-1-dev git python2.7 clang make libx11-dev libxkbfile-dev fakeroot rpm
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 echo "Sourcing scripts..."
